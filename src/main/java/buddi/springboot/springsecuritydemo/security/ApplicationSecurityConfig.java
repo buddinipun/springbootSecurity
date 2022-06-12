78ce5afa7b16f873ba9599dcf85747a99981d994
buddinipun <buddi@Buddhikas-MacBook-Air.local>
2022-06-12 23:16:19 +0530

    method level permit management

--- src/main/java/buddi/springboot/springsecuritydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/buddi/springboot/springsecuritydemo/security/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,6 +19,7 @@ import static buddi.springboot.springsecuritydemo.security.ApplicationUserRoles.
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
@@ -35,10 +37,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                .antMatchers("/", "index", "/css/*", "js/*").permitAll()
                .antMatchers("/api/**").hasRole(STUDENT.name())
                //define authority and permissions
-               .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermissioons())
-               .antMatchers(HttpMethod.POST,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermissioons())
-               .antMatchers(HttpMethod.PUT,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermissioons())
-               .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(),ADMINISTRATOR.name())
+//               .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermissioons())
+//               .antMatchers(HttpMethod.POST,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermissioons())
+//               .antMatchers(HttpMethod.PUT,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermissioons())
+//               .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(),ADMINISTRATOR.name())
                .anyRequest()
                .authenticated()
                .and()
--- src/main/java/buddi/springboot/springsecuritydemo/student/StudentManagementController.java
+++ src/main/java/buddi/springboot/springsecuritydemo/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package buddi.springboot.springsecuritydemo.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -14,23 +15,28 @@ public class StudentManagementController {
             new Student(2, "Maria Jones"),
             new Student(3, "Anna Smith")
     );
-
+// hasRole('ROLE_')     hasAnyRole('ROLE_')     hasAuthority('permission')     hassAnyAuthority('permission')
+    // method level permissions
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINISTRATOR')")
     public List<Student> gtAllStudents() {
         System.out.println("gtAllStudents ");
         return STUDENTS;
     }
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println("registerNewStudent ");
         System.out.println(student);
     }
     @DeleteMapping(path="{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentID) {
         System.out.println("deleteStudent ");
         System.out.println(studentID);
     }
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId,@RequestBody Student student){
         System.out.println("updateStudent ");
         System.out.println(String.format("%s %s",studentId,student));
